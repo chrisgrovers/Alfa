5569e67e578961435d6484f1f6e4a131227f8bcf
chrisgrovers <chrisgrovers@gmail.com>
2015-11-11 00:19:06 -0800

    [first commit] playing with template strings

---
+++ ES6Practice/es6.js
@@ -0,0 +1,9 @@
+console.log(`string text line 1
+string text line 2`);
+//string text line 1
+//string text line 2
+var a = 5;
+var b = 10;
+console.log(`Fifteen is ${a + b} and\nnot ${2 * a + b}.`);
+//es6.js:6 Fifteen is 15 and
+//not 20.
